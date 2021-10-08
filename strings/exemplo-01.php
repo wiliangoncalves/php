74394d07c98b81a50dc8ff5a3c84019673f92ff5
wilian gonçalves <wwiliangoncalves@gmail.com>
2021-10-08 12:40:21 -0300

    Algumas funções para manipular strings em php

---
+++ strings/exemplo-01.php
@@ -0,0 +1,8 @@
+<?php
+    $nome = "wile";
+    $nome2 = 'wilian';
+
+    //var_dump($nome, $nome2);
+
+    echo "ABC $nome";
+?>
---
+++ strings/exemplo-02.php
@@ -0,0 +1,25 @@
+<?php
+    $nome = "hanriel alinsky";
+
+    $nome = strtoupper($nome);
+
+    echo $nome;
+
+    $nome =  strtolower($nome);
+
+    echo "<br/>";
+
+    echo $nome;
+
+    echo "<br/>";
+
+    $nome = ucfirst($nome);
+
+    echo $nome;
+
+    echo "<br/>";
+
+    $nome = ucwords($nome);
+
+    echo $nome;
+?>
